1a035158f4e330186b22c77301016f8bd4e57e45
Nakamura <n.aoi.pg@gmail.com>
2020-05-07 12:51:12 +0900

    dune build / dune installを可能にした

--- output/test.py
+++ output/test.py
@@ -1,19 +1,5 @@
-m_local__x_2 = None
-m_local__xs_3 = None
-def a():
-	return (lambda f_local__l_1:(("even") if (match(f_local__l_1,{'Nil':None,})) else ((((b()) (m_local__xs_3)) if (match(f_local__l_1,{'Cons':("m_local__x_2","m_local__xs_3"),})) else (exit())))))
-m_local__x_2 = None
-m_local__xs_3 = None
-def b():
-	return (lambda f_local__l_1:(("odd") if (match(f_local__l_1,{'Nil':None,})) else ((((a()) (m_local__xs_3)) if (match(f_local__l_1,{'Cons':("m_local__x_2","m_local__xs_3"),})) else (exit())))))
-def _local__p_4(_ctx):
-	return (lambda f_local__x_6:{'Cons':(1,((_ctx["_local__q_4"]) ({'f_local__x_6':f_local__x_6,'_local__p_4':_ctx["_local__p_4"],'_local__q_4':_ctx["_local__q_4"],})) (f_local__x_6)),})
-def _local__q_4(_ctx):
-	return (lambda f_local__x_5:{'Cons':(2,((_ctx["_local__p_4"]) ({'f_local__x_5':f_local__x_5,'_local__p_4':_ctx["_local__p_4"],'_local__q_4':_ctx["_local__q_4"],})) (f_local__x_5)),})
 def f():
-	return ((_local__p_4) ({'_local__p_4':_local__p_4,'_local__q_4':_local__q_4,}))
-def main():
-	return ((a()) ({'Cons':(1,{'Cons':(2,{'Cons':(3,{'Cons':(4,{'Nil':None,}),}),}),}),}))
+	return (3)
 
 def match(pat1, pat2):
 	if (type(pat2) is dict) and (type(pat1) is dict):
